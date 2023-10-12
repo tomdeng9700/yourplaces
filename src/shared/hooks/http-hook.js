d3ad7d4751de7fe1f3b9ba86b184bbbd72262e4f
Shang Jian Deng <sjdeng@uwaterloo.ca>
2023-10-11 23:05:00 -0400

    remove log from http-hook

--- src/shared/hooks/http-hook.js
+++ src/shared/hooks/http-hook.js
@@ -29,7 +29,6 @@ export const useHttpClient = () => {
             setIsLoading(false);
             return responseData;
         } catch (err) {
-            console.log(err);
             setError(err.message);
             setIsLoading(false);
             throw err;
